a44a131ef3c737deb9068066a5fbfe05a6157de0
kiwi <ultrakiwi99@gmail.com>
2023-12-23 19:42:36 +0200

    Fonts setup

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import {Inter} from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+
+export const lustiana = Lusitana({subsets: ['latin'], weight: ['400', '700']})
