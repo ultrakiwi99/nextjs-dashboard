06b7d150f290437dec7fe87423443f5d0b5d7fa0
kiwi <ultrakiwi99@gmail.com>
2024-01-01 19:17:36 +0200

    Metadata for all significant pages

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,10 @@
 import { ReactNode } from 'react';
 import SideNav from '../ui/dashboard/sidenav';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
